cec47e0f085a8a66f1daba997f300a5de56c5226
emoreno-94 <edward_moreno_94@hotmail.com>
2015-06-05 05:39:26 -0300

    parser para salida del proceso de analisis de CheckStyle

---
+++ modules/CheckStyle/src/main/java/utility/XmlParser.java
@@ -0,0 +1,47 @@
+package utility;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public abstract class XmlParser {
+	
+	ArrayList<DataColour> data;
+	
+	public XmlParser() {
+		data = new ArrayList<DataColour>();
+	}
+	
+	public void parse(String path) {
+		try {
+			//abrir archivo de salida
+			File fXmlFile = new File(path);
+
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(fXmlFile);
+
+			doc.getDocumentElement().normalize();
+
+			NodeList files = doc.getElementsByTagName("file");
+			parseFiles(files);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public abstract void parseFiles(NodeList files); 
+	
+	public String toString() {
+		StringBuilder str = new StringBuilder();
+		for (DataColour dataColour : data) {
+			str.append(dataColour + "\n");
+		}
+		return str.toString();
+	}
+}
