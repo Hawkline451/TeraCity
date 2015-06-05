87a3b97d69236e5737e32234b2bdb588843f9ae3
emoreno-94 <edward_moreno_94@hotmail.com>
2015-06-05 05:31:35 -0300

    agregado nuevo archivo para pruebas

---
+++ modules/CheckStyle/Project/In/File3.java
@@ -0,0 +1,10 @@
+public class File3 {
+	public static void main(String[] args) {
+		if (true || false || false);
+		if (true || false || false || false);
+		if (true || false || false || false || false );
+		if (true || false || false || false || false || false);
+		if (true || false || false || false || false || false || false);
+		if (true || false || false || false || false || false || false || false);
+	}
+}
