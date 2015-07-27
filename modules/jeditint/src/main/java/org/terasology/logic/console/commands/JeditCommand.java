61058a94980ddb70e7f6f94c686452c15e769404
Phfollert <phfollert@gmail.com>
2015-07-27 05:42:42 -0300

    Doesn't open jedit multiple times and CodeCity folder works in Windows

--- modules/jeditint/src/main/java/org/terasology/logic/console/commands/JeditCommand.java
+++ modules/jeditint/src/main/java/org/terasology/logic/console/commands/JeditCommand.java
@@ -13,19 +13,22 @@ import org.terasology.logic.console.commandSystem.annotations.CommandParam;
 public class JeditCommand  extends BaseComponentSystem {
 	 @Command( shortDescription = "Open jedit", helpText = "Open jedit in the class of the selected structure" )
      public String jedit(@CommandParam("Class") String className) {
-		 String baseFolder = "~/Escritorio/Terasology/CodeCity/";
 		 String osName = System.getProperty("os.name" );
+		 String baseFolder = System.getProperty("user.dir");
+		 String dir = baseFolder+"/CodeCity/"+className;
 		 Process p;
+		 
 		 try{
 			 if (osName.contains("Windows")){
-				 String[] cmd = new String[3];
+				 String[] cmd = new String[4];
 				 cmd[0] ="cmd.exe";
 				 cmd[1] = "/C";
-				 cmd[2] = "java -jar C:/\"Program Files\"/jedit/jedit.jar "+baseFolder+className;
+				 cmd[2] = "\"C:/Program Files/jedit/jedit.exe\"";
+				 cmd[3] = dir;
 				 p=Runtime.getRuntime().exec(cmd);
 			 }
 			 else{
-				 String cmd = "jedit "+baseFolder+className;
+				 String cmd = "jedit "+dir;
 				 p=Runtime.getRuntime().exec(cmd);
 			 }
 		     return "Work";
