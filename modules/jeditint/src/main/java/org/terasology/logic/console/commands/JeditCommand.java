002c847f35585dd8a972390e6f081ef5fed4e829
Phfollert <phfollert@gmail.com>
2015-07-27 04:33:18 -0300

    JeditCommand now works with windows,but opens jedit every time is called

--- modules/jeditint/src/main/java/org/terasology/logic/console/commands/JeditCommand.java
+++ modules/jeditint/src/main/java/org/terasology/logic/console/commands/JeditCommand.java
@@ -14,14 +14,25 @@ public class JeditCommand  extends BaseComponentSystem {
 	 @Command( shortDescription = "Open jedit", helpText = "Open jedit in the class of the selected structure" )
      public String jedit(@CommandParam("Class") String className) {
 		 String baseFolder = "~/Escritorio/Terasology/CodeCity/";
+		 String osName = System.getProperty("os.name" );
+		 Process p;
 		 try{
-		        String cmd = "jedit "+baseFolder+className;
-		        Process p=Runtime.getRuntime().exec(cmd);
-		        return "Work";
+			 if (osName.contains("Windows")){
+				 String[] cmd = new String[3];
+				 cmd[0] ="cmd.exe";
+				 cmd[1] = "/C";
+				 cmd[2] = "java -jar C:/\"Program Files\"/jedit/jedit.jar "+baseFolder+className;
+				 p=Runtime.getRuntime().exec(cmd);
+			 }
+			 else{
+				 String cmd = "jedit "+baseFolder+className;
+				 p=Runtime.getRuntime().exec(cmd);
+			 }
+		     return "Work";
 		        
 		 }
 		 catch(IOException e1) {
-			 return e1.toString();
+			 return "You must install jedit first";
 		 }
 	 }
 }
