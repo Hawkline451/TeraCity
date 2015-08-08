8b92908b4265d1b2adaf18a7980c251fe76fb18a
emoreno-94 <edward.moreno@ing.uchile.cl>
2015-08-07 23:51:43 -0300

    Agregadas funcionalidades a los botones
    Cada boton realiza su analisis con ruta y valor por defecto, la ruta es
    modules/CheckStyle/Project/In y el valor depende de cada metrica

--- engine/src/main/java/org/terasology/rendering/nui/layers/mainMenu/CheckStyleMenuScreen.java
+++ engine/src/main/java/org/terasology/rendering/nui/layers/mainMenu/CheckStyleMenuScreen.java
@@ -15,11 +15,17 @@
  */
 package org.terasology.rendering.nui.layers.mainMenu;
 
+import java.util.ArrayList;
+
 import org.terasology.asset.AssetType;
 import org.terasology.asset.AssetUri;
 import org.terasology.asset.Assets;
 import org.terasology.config.Config;
-import org.terasology.logic.console.commands.CoreCommands;
+import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.logic.console.Console;
+import org.terasology.logic.console.commandSystem.ConsoleCommand;
+import org.terasology.logic.console.commandSystem.exceptions.CommandExecutionException;
+import org.terasology.naming.Name;
 import org.terasology.registry.In;
 import org.terasology.rendering.nui.CoreScreenLayer;
 import org.terasology.rendering.nui.UIWidget;
@@ -28,7 +34,6 @@ import org.terasology.rendering.nui.asset.UIData;
 import org.terasology.rendering.nui.asset.UIElement;
 import org.terasology.rendering.nui.layers.mainMenu.inputSettings.InputSettingsScreen;
 import org.terasology.rendering.nui.widgets.ActivateEventListener;
-import org.terasology.rendering.nui.widgets.UIText;
 
 public class CheckStyleMenuScreen extends CoreScreenLayer {
 
@@ -36,6 +41,9 @@ public class CheckStyleMenuScreen extends CoreScreenLayer {
 
     @In
     private Config config;
+    
+    @In
+    private Console console;
 
     @Override
     public void initialise() {
@@ -43,39 +51,90 @@ public class CheckStyleMenuScreen extends CoreScreenLayer {
         inputScreen.setSkin(getSkin());
         UIData inputScreenData = new UIData(inputScreen);
         Assets.generateAsset(INPUT_SCREEN_URI, inputScreenData, UIElement.class);
+        
         WidgetUtil.trySubscribe(this, "ciclomatica", new ActivateEventListener() {
             @Override
             public void onActivated(UIWidget widget) {
-            	CoreCommands c = new CoreCommands();
-            	c.fullscreen();
+            	ConsoleCommand ca = console.getCommand(new Name("cstyle"));
+            	ArrayList<String> params = new ArrayList<String>();
+            	params.add("modules/CheckStyle/Project/In"); 
+            	params.add("-c");
+            	params.add("10");
+            	EntityRef e = null;
+            	try {
+					ca.execute(params, e);
+					System.out.println("\n C: \n");
+				} catch (CommandExecutionException e1) {
+					System.out.println("\nNo se pudo realizar el análisis\n");
+				}
             }
         });
         WidgetUtil.trySubscribe(this, "booleana", new ActivateEventListener() {
             @Override
             public void onActivated(UIWidget widget) {
-            	CoreCommands c = new CoreCommands();
-            	c.fullscreen();
+            	ConsoleCommand ca = console.getCommand(new Name("cstyle"));
+            	ArrayList<String> params = new ArrayList<String>();
+            	params.add("modules/CheckStyle/Project/In"); 
+            	params.add("-b");
+            	params.add("3");
+            	EntityRef e = null;
+            	try {
+					ca.execute(params, e);
+					System.out.println("\n C: \n");
+				} catch (CommandExecutionException e1) {
+					System.out.println("\nNo se pudo realizar el análisis\n");
+				}
             }
         });
         WidgetUtil.trySubscribe(this, "fanOut", new ActivateEventListener() {
             @Override
             public void onActivated(UIWidget widget) {
-            	CoreCommands c = new CoreCommands();
-            	c.fullscreen();
+            	ConsoleCommand ca = console.getCommand(new Name("cstyle"));
+            	ArrayList<String> params = new ArrayList<String>();
+            	params.add("modules/CheckStyle/Project/In"); 
+            	params.add("-f");
+            	params.add("10");
+            	EntityRef e = null;
+            	try {
+					ca.execute(params, e);
+					System.out.println("\n C: \n");
+				} catch (CommandExecutionException e1) {
+					System.out.println("\nNo se pudo realizar el análisis\n");
+				}
             }
         });
         WidgetUtil.trySubscribe(this, "nPath", new ActivateEventListener() {
             @Override
             public void onActivated(UIWidget widget) {
-            	CoreCommands c = new CoreCommands();
-            	c.fullscreen();
+            	ConsoleCommand ca = console.getCommand(new Name("cstyle"));
+            	ArrayList<String> params = new ArrayList<String>();
+            	params.add("modules/CheckStyle/Project/In"); 
+            	params.add("-n");
+            	params.add("1000");
+            	EntityRef e = null;
+            	try {
+					ca.execute(params, e);
+					System.out.println("\n C: \n");
+				} catch (CommandExecutionException e1) {
+					System.out.println("\nNo se pudo realizar el análisis\n");
+				}
             }
         });
         WidgetUtil.trySubscribe(this, "dataAbstractionCoupling", new ActivateEventListener() {
             @Override
             public void onActivated(UIWidget widget) {
-            	CoreCommands c = new CoreCommands();
-            	c.fullscreen();
+            	ConsoleCommand ca = console.getCommand(new Name("cstyle"));
+            	ArrayList<String> params = new ArrayList<String>();
+            	params.add("modules/CheckStyle/Project/In"); 
+            	params.add("-d");
+            	params.add("5");
+            	EntityRef e = null;
+            	try {
+					ca.execute(params, e);
+					System.out.println("\n C: \n");
+				} catch (CommandExecutionException e1) {
+					System.out.println("\nNo se pudo realizar el análisis\n");
+				}
             }
         });
         WidgetUtil.trySubscribe(this, "close", new ActivateEventListener() {
