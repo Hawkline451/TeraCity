6db9514dedb3d8252b35eb7e65d3d3965a726274
emoreno-94 <edward.moreno@ing.uchile.cl>
2015-08-11 23:06:31 -0300

    Refactoring para soportar esperas de datos
    Se agregaron atributos a la clase Metric con el fin de que se pudieran
    recuperar los datos parseados de los analisis
    
    Autores: Edward Moreno, Sebastián Ormeño

--- modules/CheckStyle/src/main/java/metrics/Metric.java
+++ modules/CheckStyle/src/main/java/metrics/Metric.java
@@ -10,17 +10,26 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import org.terasology.logic.console.Console;
+import org.terasology.registry.In;
+
+import utility.CheckStyleParser;
+import utility.DataColour;
+
 
 public abstract class Metric{
 	String path = "modules/CheckStyle/libs/CheckStyle/Metrics/";
 	String jar = "modules/CheckStyle/libs/CheckStyle/checkstyle-6.6-all.jar";
 	int max;
+	Thread thread;
+	MetricExecution me;
+	
+	@In
+	Console console;
 	
 	public Metric() {
-		
 	}
 	
-	public static Metric createMetric(String StringMetric, int max) throws IOException {
+	public static Metric createMetric(String StringMetric, int max, Console console) throws IOException {
 		Metric metric;
 		if (StringMetric.equals("-b")) metric = new BooleanMetric();
 		else if (StringMetric.equals("-c")) metric = new CyclomaticMetric();
@@ -29,12 +38,17 @@ public abstract class Metric{
 		else if (StringMetric.equals("-n")) metric = new NPathMetric();
 		else metric = new NullMetric();
 		
+		metric.setConsole(console);
 		metric.setMax(max);
 		ArrayList<String> linesArray = metric.getMetricText();
 		metric.setMetricValue(max, linesArray);
 		return metric;
 	}
 	
+	private void setConsole(Console console) {
+		this.console = console;
+	}
+
 	private void setMax(int max) {
 		this.max = max;
 	}
@@ -87,18 +101,31 @@ public abstract class Metric{
 		}
 		return lines;
 	}
-
-	public void execute(String pathFile, Console console) throws IOException {
+	
+	public void execute(String pathFile) {
 		String commandJar = createJarCommand(pathFile);
-		Thread thread = new Thread(new MetricExecution(commandJar, pathFile, console));
-		console.addMessage("Comenzó el análisis....");
+		me = new MetricExecution(commandJar, pathFile, console);
+		thread = new Thread(me);
+		if (console != null) console.addMessage("Comenzó el análisis....");
+		else System.out.println("Comenzó el análisis....");
 		thread.start();
+	}
+	
+	public ArrayList<DataColour> getData() throws InterruptedException {
+		if (console != null) console.addMessage("Esperando termino del análisis...");
+		else System.out.println("Esperando termino del análisis...");
+		thread.join();
+		return me.data;
 	}
 }
 
-class MetricExecution implements Runnable {
+
+class MetricExecution implements Runnable{
 	String path;
 	String commandJar;
+	ArrayList<DataColour> data;
+	
+	@In
 	Console console;
 	
 	public MetricExecution(String commandJar, String path, Console console) {
@@ -107,17 +134,33 @@ class MetricExecution implements Runnable {
 		this.console = console;
 	}
 	
+	public void analize() throws IOException {
+		Process process = Runtime.getRuntime().exec(commandJar);
+		InputStream is = process.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		BufferedReader br = new BufferedReader(isr);
+		while (br.readLine()!=null);
+	}
+	
+	public void parse() {
+		String path = "modules/CheckStyle/Project/out.xml";
+		CheckStyleParser cp = new CheckStyleParser();
+		cp.parse(path);
+		data = cp.getData();
+	}
+
 	@Override
 	public void run() {
 		try {
-			Process process = Runtime.getRuntime().exec(commandJar);
-			InputStream is = process.getInputStream();
-			InputStreamReader isr = new InputStreamReader(is);
-			BufferedReader br = new BufferedReader(isr);
-			while (br.readLine()!=null);
-			console.addMessage("Terminó el análisis");
-		} catch (Exception e) {
-			console.addMessage("Falló el análisis");
+			analize();
+			parse();
+			if (console != null) console.addMessage("Terminó el análisis");
+			else System.out.println("Terminó el análisis");
+		} catch (IOException e) {
+			if (console != null) console.addMessage("Falló el analisis");
+			else System.err.println("Falló el analisis");
+			e.printStackTrace();
 		}
+		
 	}
 }
