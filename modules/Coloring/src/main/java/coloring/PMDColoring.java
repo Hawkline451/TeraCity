577b48024ebc2700add71baa678d062931e07433
Manuel Cáceres Reyes <el.ariel.cl@gmail.com>
2015-08-12 17:38:22 -0300

    Implementacion metodo getColor

--- modules/Coloring/src/main/java/coloring/PMDColoring.java
+++ modules/Coloring/src/main/java/coloring/PMDColoring.java
@@ -1,12 +1,15 @@
 package coloring;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PMDColoring extends AbstractColoring{
-
+	Map<String,String> colors = new HashMap<String,String>();
+	
 	@Override
 	public String getColor(String path) {
-		return null;
+		return colors.get(path);
 	}
 
 	@Override
