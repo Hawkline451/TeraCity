1288d3ce94affad3649030f6dcfae95106b586a2
Manuel Cáceres Reyes <el.ariel.cl@gmail.com>
2015-08-12 18:41:27 -0300

    Invirtiendo dependencia

--- modules/Coloring/src/main/java/coloring/PMDColoring.java
+++ modules/Coloring/src/main/java/coloring/PMDColoring.java
@@ -1,19 +1,28 @@
 package coloring;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.terasology.codecity.world.structure.CodeRepresentation;
+import org.terasology.registry.CoreRegistry;
+
 public class PMDColoring extends AbstractColoring{
 	Map<String,String> colors = new HashMap<String,String>();
+	PMDProcessor pmdP;
+	String rootPath = CoreRegistry.get(CodeRepresentation.class).getPath();
 	
 	@Override
 	public String getColor(String path) {
 		return colors.get(path);
 	}
-
+	
 	@Override
 	public void getDataColoring() throws IOException {
-		
+
+		if (pmdP == null ||  !(rootPath.equals(CoreRegistry.get(CodeRepresentation.class).getPath())))
+			pmdP = new PMDProcessor(rootPath);
+		colors = pmdP.getMap();
 	}
 }
---
+++ modules/Coloring/src/main/java/coloring/PMDProcessor.java
@@ -0,0 +1,15 @@
+package coloring;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PMDProcessor {
+
+	public PMDProcessor(String rootPath) {
+	}
+
+	public Map<String, String> getMap() {
+		return new HashMap<String, String>();
+	}
+}
