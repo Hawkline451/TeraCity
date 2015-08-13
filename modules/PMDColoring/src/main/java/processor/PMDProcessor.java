dc42033b23ba16d16097bca41851dc2b9e7851a5
Manuel Cáceres Reyes <el.ariel.cl@gmail.com>
2015-08-13 09:59:09 -0300

    Eliminacion de lineas innecesarias

--- modules/PMDColoring/src/main/java/processor/PMDProcessor.java
+++ modules/PMDColoring/src/main/java/processor/PMDProcessor.java
@@ -114,48 +114,4 @@ public class PMDProcessor {
 		return new DefaultMetric();
 	}
 	
-/*//FIRST COUNT OCURRENCES, THEN ASSIGN A COLOR
-	private void process() {
-		String inputString = buildInputString();
-		try {
-			Process process;
-			process = Runtime.getRuntime().exec(inputString);
-			InputStream is = process.getInputStream();
-			 InputStreamReader isr = new InputStreamReader(is);
-			 BufferedReader br = new BufferedReader(isr);
-				
-			 String line;
-			 int messageLines = 0;
-			 while ((line = br.readLine()) != null) 
-			 {
-				 //console.addMessage(line);
-				 ++messageLines;
-			 }
-			 console.addMessage("Lineas del mensage: "+ messageLines);
-			 
-			 int totalLines = new LineCounter(LineCounter.JAVA_REGEX).countLines(sourcePath);
-			 
-			 console.addMessage("Lineas totales: "+ totalLines);
-			 if(rule.equals("comments"))
-			 {
-				 String color = new CommentsMetric(messageLines, totalLines).getColor();
-				 currentColor = color;
-				 console.addMessage(color);
-			 }
-			 else if(rules.equals("codesize"))
-			 {
-				 String color = new CodeSizesMetric(messageLines, totalLines).getColor();
-				 currentColor = color;
-				 console.addMessage(color);
-			 }
-			 console.addMessage("Fin del Analisis");
-		}
-		catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
-*/
-
 }
