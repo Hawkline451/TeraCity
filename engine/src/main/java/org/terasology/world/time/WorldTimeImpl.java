e3db5b75991b16201a40dfb81cf668c6ee105b90
Ignacio Cuevas <nacho_boom_7stars@hotmail.com>
2015-12-03 12:08:52 -0300

    Suppresed flow of time. Added commands to output and set time. Part of Issue #50.

--- engine/src/main/java/org/terasology/world/time/WorldTimeImpl.java
+++ engine/src/main/java/org/terasology/world/time/WorldTimeImpl.java
@@ -22,6 +22,8 @@ import org.terasology.entitySystem.entity.EntityManager;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.UpdateSubscriberSystem;
+import org.terasology.logic.console.commandSystem.annotations.Command;
+import org.terasology.logic.console.commandSystem.annotations.CommandParam;
 import org.terasology.registry.In;
 import org.terasology.world.WorldComponent;
 
@@ -30,7 +32,7 @@ import org.terasology.world.WorldComponent;
  */
 public class WorldTimeImpl extends BaseComponentSystem implements WorldTime, UpdateSubscriberSystem {
 
-    private static final float WORLD_TIME_MULTIPLIER = 48f;
+    private static final float WORLD_TIME_MULTIPLIER = /*48f*/0f;
 
     private AtomicLong worldTime = new AtomicLong(0);
 
@@ -39,7 +41,15 @@ public class WorldTimeImpl extends BaseComponentSystem implements WorldTime, Upd
 
     @In
     private EntityManager entityManager;
-
+    
+    public WorldTimeImpl() {
+    	this(43200);
+    }
+    
+    public WorldTimeImpl(long initialTime) {
+    	this.setTime(initialTime);
+    }
+    
     @Override
     public long getMilliseconds() {
         return worldTime.get();
@@ -49,7 +59,7 @@ public class WorldTimeImpl extends BaseComponentSystem implements WorldTime, Upd
     public float getSeconds() {
         return worldTime.get() / 1000f;
     }
-
+    
     @Override
     public float getDays() {
         return worldTime.get() / (float) DAY_LENGTH;
@@ -87,7 +97,6 @@ public class WorldTimeImpl extends BaseComponentSystem implements WorldTime, Upd
                 getWorldEntity().send(new WorldTimeEvent(tick));
             }
 
-            // TODO: consider sending a DailyTick (independent from solar events such as midnight)
         }
     }
 
@@ -97,4 +106,21 @@ public class WorldTimeImpl extends BaseComponentSystem implements WorldTime, Upd
         }
         return EntityRef.NULL;
     }
+    
+	@Command( shortDescription = "Displays time", helpText = "Displays world time (in milliseconds)" )
+    public String time() {
+        long currentTime = (worldTime.get() % DAY_LENGTH) / 1000;
+        int hours = (int) ((currentTime / 60) / 60) % 24;
+        int minutes = (int) (currentTime / 60) % 60;
+        int seconds = (int) currentTime % 60;
+        return ((hours < 10) ? "0" + hours : hours) + " : " +
+        		((minutes < 10) ? "0" + minutes : minutes) + " : " + 
+        		((seconds < 10) ? "0" + seconds : seconds);
+    }
+	
+	@Command( shortDescription = "Set world time", helpText = "Receives a value (in seconds) and sets the world time to it")
+	public String setTime(@CommandParam( value = "New time" ) Long newTime) {
+		this.setMilliseconds((newTime * 1000) % DAY_LENGTH);
+		return "Set time to " + time();
+	}
 }
