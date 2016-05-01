d639d9dcd44ed1c522f270ffd6daee393649a420
JuanRE <jrojasespejo@gmail.com>
2016-05-01 15:05:41 -0300

    implementacion de la fabrica de texturas

--- engine/src/main/java/org/terasology/world/block/BlockTextureFactory.java
+++ engine/src/main/java/org/terasology/world/block/BlockTextureFactory.java
@@ -1,5 +1,9 @@
 package org.terasology.world.block;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.util.HashMap;
 
@@ -9,6 +13,9 @@ import java.util.HashMap;
  */
 public final class BlockTextureFactory {
 
+  private static final int MAXLENGTH = 16;
+  private static final int RESOLUTION = 16;
+
   private static BlockTextureFactory fact;
   private HashMap<String, BufferedImage> pics;
 
@@ -16,15 +23,19 @@ public final class BlockTextureFactory {
     pics = new HashMap<String, BufferedImage>();
   }
 
+
+
   public static BlockTextureFactory getFactory() {
     if (fact == null)
       fact = new BlockTextureFactory();
     return fact;
   }
-  
 
+  /**
+   * Defaults the pos an total parameters to 1.
+   */
   public BufferedImage getImage(int line1, int line2) {
-    return getImage(line1,line2,1,1);
+    return getImage(line1, line2, 1, 1);
   }
 
   /**
@@ -44,6 +55,72 @@ public final class BlockTextureFactory {
    * @return Texture image
    */
   public BufferedImage getImage(int line1, int line2, int pos, int total) {
+    line1 = trimLine(line1, pos, total);
+    line2 = trimLine(line2, pos, total);
+    if (!pics.containsKey(line1 + "_" + line2)) {
+      createImage(line1, line2);
+    }
+    return pics.get(line1 + "_" + line2);
+  }
+
+  private void createImage(int line1, int line2) {
+    String text = generateImageText(line1, line2);
+
+    Font font = new Font("Arial", Font.PLAIN, 3);
+    BufferedImage image = new BufferedImage(RESOLUTION, RESOLUTION, BufferedImage.TYPE_INT_ARGB);
+    Graphics2D g2d = image.createGraphics();
+    g2d.setFont(font);
+    FontMetrics fm = g2d.getFontMetrics();
+    g2d.setColor(Color.WHITE);
+    g2d.fillRect(0, 0, 16, 16);
+    g2d.setColor(Color.BLACK);
+
+    String[] split = text.split("\n");
+    int y = 0;
+    for (String str : split) {
+      g2d.drawString(str, 0, y += fm.getAscent());
+    }
+    g2d.dispose();
+
+    pics.put(line1 + "_" + line2, image);
+  }
+
+  private String generateImageText(int line1, int line2) {
+    StringBuilder sb = new StringBuilder();
+    sb.append(" \n");
+    for (int i = 0; i < line1; i++) {
+      sb.append("=");
+    }
+    sb.append("\n \n");
+    for (int i = 0; i < line2; i++) {
+      sb.append("=");
+    }
+    sb.append("\n ");
+    return sb.toString();
+  }
+
+  private int trimLine(int line, int pos, int total) {
+    double max = MAXLENGTH;
+    if (line + 1 < max / total * (pos - 1)) {
+      return 0;
+    } else if (line + 1 < max / total * (pos)) {
+      return (int) (total * (line + 1 - max / total * (pos - 1)));
+    } else {
+      return (int) MAXLENGTH;
+    }
+  }
+
+  /**
+   * Paints the texture to the desired colour for metrics.
+   * 
+   * @param pic
+   *          Picture to paint.
+   * @param colour
+   *          Color to paint the picture.
+   * @return Painted picture.
+   */
+  public BufferedImage paintTexture(BufferedImage pic, String colour) {
+    //TODO implement.
     return null;
   }
 }
