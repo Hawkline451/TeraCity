7f444348636b8f299017eea8ccdaf1b848082f3a
fcopulgar <retri.francisco@gmail.com>
2016-05-31 10:39:01 -0400

    Add module to capture when pressed the key K

---
+++ modules/jeditint/src/main/java/org/terasology/logic/jedit/EditClassSystem.java
@@ -0,0 +1,39 @@
+package org.terasology.logic.jedit;
+
+import org.terasology.codecity.world.map.CodeMap;
+import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.entitySystem.event.ReceiveEvent;
+import org.terasology.entitySystem.systems.BaseComponentSystem;
+import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.input.ButtonState;
+import org.terasology.input.binds.general.ClassEditButton;
+import org.terasology.input.cameraTarget.CameraTargetSystem;
+import org.terasology.network.ClientComponent;
+import org.terasology.registry.CoreRegistry;
+import org.terasology.registry.In;
+import org.terasology.utilities.jedit.JeditManager;
+
+/**
+ * Edit Class inside Minecraft
+ * @author Francisco Pulgar
+ */
+@RegisterSystem
+public class EditClassSystem extends BaseComponentSystem {
+	
+	 @In
+	 private CameraTargetSystem cameraTarget;
+	
+	 /**
+	  * edit Class inside of Minecraft of the TargetBlock MapObject		
+	  * @param event
+	  * @param entity
+	  */
+	 @ReceiveEvent(components = ClientComponent.class)
+	 public void openJedit(ClassEditButton event, EntityRef entity) {
+		 if (event.getState() == ButtonState.DOWN) {
+			 JeditManager.editClassWhenPressed(cameraTarget);
+	         event.consume();
+	    }
+	 }
+}
+
