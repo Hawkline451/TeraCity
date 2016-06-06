11d64eff81a907d39e0599051f4cb649788135b5
fcopulgar <retri.francisco@gmail.com>
2016-06-06 16:04:10 -0400

    Refactor EditClassButton

--- modules/jeditint/src/main/java/org/terasology/logic/jedit/EditClassSystem.java
+++ modules/jeditint/src/main/java/org/terasology/logic/jedit/EditClassSystem.java
@@ -1,5 +1,8 @@
 package org.terasology.logic.jedit;
 
+import org.terasology.asset.AssetType;
+import org.terasology.asset.AssetUri;
+import org.terasology.asset.Assets;
 import org.terasology.codecity.world.map.CodeMap;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.event.ReceiveEvent;
@@ -7,10 +10,16 @@ import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterSystem;
 import org.terasology.input.ButtonState;
 import org.terasology.input.binds.general.EditClassButton;
+import org.terasology.input.binds.general.PauseButton;
 import org.terasology.input.cameraTarget.CameraTargetSystem;
 import org.terasology.network.ClientComponent;
 import org.terasology.registry.CoreRegistry;
 import org.terasology.registry.In;
+import org.terasology.rendering.nui.ControlWidget;
+import org.terasology.rendering.nui.NUIManager;
+import org.terasology.rendering.nui.asset.UIData;
+import org.terasology.rendering.nui.asset.UIElement;
+import org.terasology.rendering.nui.layers.ingame.inventory.TransferItemCursor;
 import org.terasology.utilities.jedit.JeditManager;
 
 /**
@@ -22,7 +31,20 @@ public class EditClassSystem extends BaseComponentSystem {
 	
 	 @In
 	 private CameraTargetSystem cameraTarget;
-	
+	 
+	 @In
+	 private NUIManager nuiManager;
+	 
+	 @Override
+	 
+	 public void initialise() {
+		 nuiManager.getHUD().addHUDElement("toolbar");
+		 TransferItemCursor cursor = new TransferItemCursor();
+		 UIElement cursorElement = Assets.generateAsset(new AssetUri(AssetType.UI_ELEMENT, "engine:transferItemCursor"), new UIData(cursor), UIElement.class);
+		 nuiManager.addOverlay(cursorElement, ControlWidget.class);
+	    }	 
+	 
+	 
 	 /**
 	  * edit Class inside of Minecraft of the TargetBlock MapObject		
 	  * @param event
@@ -32,8 +54,10 @@ public class EditClassSystem extends BaseComponentSystem {
 	 public void openJedit(EditClassButton event, EntityRef entity) {
 		 if (event.getState() == ButtonState.DOWN) {
 			 JeditManager.editClassWhenPressed(cameraTarget);
+			 nuiManager.toggleScreen("engine:pauseMenu");
 	         event.consume();
-	    }
+	    } 
 	 }
+	 
 }
 
