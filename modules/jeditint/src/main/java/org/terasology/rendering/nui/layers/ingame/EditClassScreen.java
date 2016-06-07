ca35dd648d2af685bce558bba067a3b615f1399a
fcopulgar <retri.francisco@gmail.com>
2016-06-06 23:55:02 -0400

    Create a Dinamic TextBox to edit

--- modules/jeditint/src/main/java/org/terasology/rendering/nui/layers/ingame/EditClassScreen.java
+++ modules/jeditint/src/main/java/org/terasology/rendering/nui/layers/ingame/EditClassScreen.java
@@ -5,22 +5,17 @@ import java.io.IOException;
 import org.terasology.codecity.world.structure.CodeRepresentation;
 import org.terasology.engine.GameEngine;
 import org.terasology.registry.CoreRegistry;
-import org.terasology.rendering.ShaderManager;
 import org.terasology.rendering.nui.CoreScreenLayer;
 import org.terasology.rendering.nui.UIWidget;
 import org.terasology.rendering.nui.WidgetUtil;
 import org.terasology.rendering.nui.widgets.ActivateEventListener;
 import org.terasology.rendering.nui.widgets.UILabel;
 import org.terasology.rendering.nui.widgets.UIText;
-import org.terasology.rendering.nui.widgets.UITextEntry;
 import org.terasology.utilities.jedit.EditClass;
 import org.terasology.utilities.jedit.JeditManager;
 import org.terasology.utilities.jedit.ClassPathVisitor;
 import org.terasology.input.cameraTarget.CameraTargetSystem;
-import org.terasology.network.ClientComponent;
-import org.terasology.registry.CoreRegistry;
 import org.terasology.registry.In;
-import org.terasology.codecity.world.structure.CodeRepresentation;
 
 /**
  * @author Francisco Pulgar Romero
@@ -54,7 +49,6 @@ public class EditClassScreen extends CoreScreenLayer{
 		try {
 			contentClass = EditClass.readFileAsString(path);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
@@ -74,7 +68,11 @@ public class EditClassScreen extends CoreScreenLayer{
             @Override
             public void onActivated(UIWidget widget) {
             	String editContent = textclass.getText();
-            	//EditClass.writeFile(pathClass.getText(), editContent);
+            	try {
+					EditClass.writeFile(pathClass.getText(), editContent);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
                 getManager().closeScreen(EditClassScreen.this);
             }
         });        
