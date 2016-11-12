6a0f0ab6a53ca39fa4ae6fb0c2a295a7e4970bf8
AlbertoSara <albsaraz@gmail.com>
2016-11-12 20:34:59 -0300

    Code smell fixes

--- engine/src/main/java/org/terasology/rendering/nui/layers/ingame/coloring/CoberturaMenuScreen.java
+++ engine/src/main/java/org/terasology/rendering/nui/layers/ingame/coloring/CoberturaMenuScreen.java
@@ -128,7 +128,7 @@ public class CoberturaMenuScreen extends CoreScreenLayer {
     	// manage invalid face selections
     	final UILabel infoField = find("infoField", UILabel.class);
     	if (face == null || color == null) {
-    		infoField.setText("waning: please choose a face and a color to paint!");
+    		infoField.setText("Warning: Please choose a face and a color to paint!");
     		return;
     	}
     	infoField.setText("");
--- engine/src/main/java/org/terasology/rendering/nui/layers/ingame/coloring/PMDMenuScreen.java
+++ engine/src/main/java/org/terasology/rendering/nui/layers/ingame/coloring/PMDMenuScreen.java
@@ -172,7 +172,7 @@ public class PMDMenuScreen extends CoreScreenLayer {
     	// manage invalid face selections
     	final UILabel infoField = find("infoField", UILabel.class);
     	if (face == null || color == null) {
-    		infoField.setText("waning: please choose a face and a color to paint!");
+    		infoField.setText("Warning: Please choose a face and a color to paint!");
     		return;
     	}
     	infoField.setText("");
