922e2f11735fd4f18c0a9297a1397faf5f36cc07
cpmeneses <cpmeneses94@gmail.com>
2016-11-19 13:10:34 -0300

    added block to TextSearchQuery

--- modules/WorldCodecity/src/main/java/org/terasology/codecity/world/glasswall/coloringAPI/TextSearchQuery.java
+++ modules/WorldCodecity/src/main/java/org/terasology/codecity/world/glasswall/coloringAPI/TextSearchQuery.java
@@ -1,11 +1,14 @@
 package org.terasology.codecity.world.glasswall.coloringAPI;
+import org.terasology.world.block.Block;
 
 public class TextSearchQuery extends AbstractColoringQuery {
+  private Block trueblock;
+  private Block falseblock;
   private String search;
   
   /**
    * TextSearchQuery(String)
-   * Initializes the query that searches for the string in search.
+   * Initializes the query that searches for the string in the variable search.
    * @param search
    */
   public TextSearchQuery(String search) {
