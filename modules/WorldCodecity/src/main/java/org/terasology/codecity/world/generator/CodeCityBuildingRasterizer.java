40db15e5b52dc033bd873d51e870e994e9e9546d
Tewi <tewisp@gmail.com>
2016-12-06 17:02:10 -0300

    fixed typo

--- modules/WorldCodecity/src/main/java/org/terasology/codecity/world/generator/CodeCityBuildingRasterizer.java
+++ modules/WorldCodecity/src/main/java/org/terasology/codecity/world/generator/CodeCityBuildingRasterizer.java
@@ -45,19 +45,16 @@ public class CodeCityBuildingRasterizer implements WorldRasterizer {
             		int row = map.getMaxY()-position.y;
             		int col = map.getColumn();
             		
+            		// Generate the invisible block related to the block code if it has code
+            		if (map.isIndexBlock()){
+            			block = CoreRegistry.get(BlockManager.class).getBlock("core:Ice");
+            		}
+            		else{
             		int[][] sliceBin = ReducedViewBlockFactory.recalcBinary(code,row,col);
             		block = ReducedViewBlockFactory.generate(sliceBin);
             		//HERE GOES THE NEW FACTORY THAT TRANSLATE BLOQUE TO THE CORRENT BLOCK
-            		
-            		// Generate the invisible block related to the block code if it has code
-            		if (map.isIndexBlock()){
-//            			if (!block.getURI().toString().matches("worldcodecity:Stone[01]+")){
-//        				if (!block.getURI().toString().contains("1")){	
-//            			if(!sliceHasCode(sliceBin)){
-//            				continue;
-//            			}
-            			block = CoreRegistry.get(BlockManager.class).getBlock("core:Ice");
             		}
+
             	    
         		}
         		else{ //Here block for borders which have map.getColumn() == -1
@@ -65,40 +62,9 @@ public class CodeCityBuildingRasterizer implements WorldRasterizer {
         		}
         		chunk.setBlock(ChunkMath.calcBlockPos(position.x, position.y, position.z), block);
         		
-//        		if(block.getURI().toString().contains("worldcodecity")){
-//        			Block lava_block = CoreRegistry.get(BlockManager.class).getBlock("core:stone");
-//        			chunk.setBlock(ChunkMath.calcBlockPos(position.x+1, position.y, position.z), lava_block);
-//        		}
-//        		
-//        		if (indexBlock != null && block.getURI().toString().contains("worldcodecity:Stone")){
-//        			
-//        			chunk.setBlock(ChunkMath.calcBlockPos(position.x+1, position.y, position.z), indexBlock);
-        			//funciona como es esperado
-        			
-//        			chunk.setBlock(ChunkMath.calcBlockPos(position.x, position.y, position.z-1), indexBlock);
-        			//con y-1 sale en todas las caras pero es raro
-//        			chunk.setBlock(ChunkMath.calcBlockPos(position.x, position.y-1, position.z), indexBlock);
-//        		}
-        		//**Con esto se buggea, cubre todo el edificio y 
-//        		Block indexBlock = IndexCodeBlockFactory.generate();
-//        		chunk.setBlock(ChunkMath.calcBlockPos(position.x, position.y-1, position.z), indexBlock);
         	}
         }
     }
     
-    private boolean sliceHasCode(int[][] sliceBin){
-    	if (sliceBin == null)
-    		return false;
-    	
-    	return (sliceBin[0][0] == 1) ||
-    			(sliceBin[0][1] == 1) ||
-    			(sliceBin[1][0] == 1) ||
-    			(sliceBin[1][1] == 1);
-//    	for (int[] a : sliceBin)
-//    		for (int i : a){
-//    			if (i > 0)
-//    				return true;
-//    		}  			
-//    	return false;
-    }
 }
+    
